268c5af1af99fcb2b7a36cc7349202d2c0a9435d
Saad Ahmed <saadahmedez1@gmail.com>
2025-01-17 21:55:24 +0200

    Add Morning Section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,3 +10,17 @@ document.getElementById("toggel-them").onclick = () => {
     document.getElementById("toggel-them").src = "assets/images/sun.webp";
   }
 };
+let morningList = document.getElementById("morning-list");
+fetch(`https://www.myjsons.com/v/b7d03829`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data["أذكار الصباح"].length);
+    let morningData = "";
+    for (let i = 0; i < data["أذكار الصباح"].length; i++) {
+      morningData += `
+      <div>
+        <p>${data["أذكار الصباح"][i]["content"]}<span>(${data["أذكار الصباح"][i]["count"]})</span></p>
+      </div>`;
+    }
+    morningList.innerHTML = morningData;
+  });
